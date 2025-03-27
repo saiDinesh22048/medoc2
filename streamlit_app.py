4226849377bb7fa9c8e7a67c78963610f0af9b07
saiDinesh22048 <118376163+saiDinesh22048@users.noreply.github.com>
2025-03-27 23:48:08 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import json
 from streamlit_webrtc import webrtc_streamer  # WebRTC for live recording
-#import whisper
-#import tempfile
+import whisper
+import tempfile
 import os
 
 """audio_value = st.audio_input("Record a voice message")
